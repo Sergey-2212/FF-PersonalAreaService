6dcc89b3de8eb6b3715bd6a513e84a4970e9cf37
LakhmatovSergei <svlakhmatov@gmail.com>
2023-07-20 06:10:09 +0300

    #PA1T30. Проведена перенастройка проекта для работы с БД H2 и отказ от flyway в пользу автогенерации со стороны Hybernate.

--- src/main/resources/db/test/V1__init.sql
+++ src/main/resources/db/test/V1__init.sql
@@ -1,27 +1,20 @@
-CREATE TYPE sex as ENUM ('male', 'female');
--- CREATE TYPE ActivityTitle as ENUM (
---     'Минимальная нагрузка',
---     'Легкая нагрузка',
---     'Умеренная нагрузка',
---     'Высокая нагрузка',
---     'Экстремальная нагрузка');
--- CREATE TYPE GoalTitle as ENUM (
---     'Поддержание веса',
---     'Похудеть',
---     'Набрать вес');
-
 CREATE TABLE IF NOT EXISTS pl_goal (
                                       goal_id serial UNIQUE,
-                                      goal_title varchar(20),
+                                      goal_title varchar(255),
                                       goal_protein real,
                                       goal_fat real,
                                       goal_carbohydrate real
 );
 
+INSERT INTO pl_goal (goal_title, goal_protein, goal_fat,goal_carbohydrate) values
+                                                                               ('Keep', 1, 1, 1),
+                                                                               ('Lose', 2, 2, 2),
+                                                                               ('Get', 3, 3, 3);
+
 
 CREATE TABLE IF NOT EXISTS pl_activity (
                                           act_id serial UNIQUE,
-                                          act_title varchar(50),
+                                          act_title varchar(255),
                                           act_coefficient real
 );
 
@@ -32,9 +25,8 @@ INSERT INTO pl_activity (act_title, act_coefficient) VALUES
 ('High', 4),
 ('Extreme', 5);
 
-CREATE TABLE IF NOT EXISTS tbl_personal_info (
-                                               inf_id bigserial UNIQUE,
-                                               inf_person_id bigint NOT NULL,
+CREATE TABLE IF NOT EXISTS tbl_person_info (
+                                               inf_id bigserial,
                                                inf_city varchar(255),
                                                inf_street varchar(255),
                                                inf_house varchar(50),
@@ -42,29 +34,33 @@ CREATE TABLE IF NOT EXISTS tbl_personal_info (
                                                inf_index INTEGER,
                                                inf_phone_number varchar(20),
                                                inf_email varchar(255),
+                                               person_id bigint,
                                                PRIMARY KEY (inf_id)
 );
 
 CREATE TABLE IF NOT EXISTS tbl_person (
                                          prs_id bigserial,
                                          prs_username varchar(255) NOT NULL,
-                                         prs_sex sex NOT NULL,
+                                         prs_sex varchar(10) NOT NULL,
                                          prs_birthdate date NOT NULL,
                                          prs_weight smallint NOT NULL,
                                          prs_height smallint NOT NULL,
                                          prs_activity_id smallint NOT NULL,
                                          prs_goal_id smallint NOT NULL,
-                                         prs_info_id bigint,
+                                         prs_person_info_id BIGINT,
                                          prs_created_at timestamp DEFAULT current_timestamp,
                                          prs_updated_at timestamp DEFAULT current_timestamp,
                                          PRIMARY KEY (prs_id),
                                          FOREIGN KEY (prs_activity_id) REFERENCES pl_activity (act_id),
                                          FOREIGN KEY (prs_goal_id) REFERENCES pl_goal(goal_id),
-                                         FOREIGN KEY (prs_info_id) REFERENCES tbl_personal_info(inf_id)
+                                         FOREIGN KEY (prs_person_info_id) REFERENCES tbl_person_info(inf_id)
+
 );
 
+ALTER TABLE tbl_person_info
+    ADD FOREIGN KEY (person_id) REFERENCES tbl_person(prs_id);
 
-ALTER TABLE tbl_personal_info
-    ADD CONSTRAINT person_personInfo_fk
-        FOREIGN KEY (inf_person_id) REFERENCES tbl_person(prs_id);
+insert into tbl_person (prs_username, prs_sex, prs_birthdate, prs_weight, prs_height, prs_activity_id, prs_goal_id, prs_person_info_id)
+    VALUES
+
 
